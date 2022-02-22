e08e2023b74db5040ec12e6cfef001c530d47424
Masao Kitamura <masaok@gmail.com>
2022-02-22 04:59:19 -0800

    Gyro demo works with new hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import useGyroscope from 'react-hook-gyroscope'
+
+const onGyroscopeUpdate = gyro => {
+  console.log('NEW GYRO INFO: ', gyro)
+}
 
 function App() {
+  const gyro = useGyroscope({ frequency: 5 }, onGyroscopeUpdate)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Gyroscope Sensor Demo</div>
+        <div>X: {gyro.x}</div>
+        <div>Y: {gyro.y}</div>
+        <div>Z: {gyro.z}</div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
